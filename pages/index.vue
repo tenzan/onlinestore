6f256888c6f10c471734686b313db93af0897037
Askar Karasaev <askar.karasaev@gmail.com>
2023-07-10 16:07:27 +0900

    temp update

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>JapanOnline.Store</h2>
+    <p>アフィリエイトサイト準備中</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
